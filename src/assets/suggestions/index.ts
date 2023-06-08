6f4ec0494e250e89f98a654816fb2fc1fda97639
Luka <lukakobaidze2006@gmail.com>
2023-06-08 12:08:22 +0400

    Moved images

--- src/assets/suggestions/index.ts
+++ src/assets/suggestions/index.ts
@@ -1,5 +1,5 @@
-export { default as backgroundHeaderDesktop } from './desktop/background-header.png';
-export { default as backgroundHeaderTablet } from './tablet/background-header.png';
-export { default as backgroundHeaderMobile } from './mobile/background-header.png';
+export { default as backgroundHeaderDesktop } from './background-header-desktop.png';
+export { default as backgroundHeaderTablet } from './background-header-tablet.png';
+export { default as backgroundHeaderMobile } from './background-header-mobile.png';
 export { default as illustrationEmpty } from './illustration-empty.svg';
 export { ReactComponent as IconSuggestions } from './icon-suggestions.svg';
